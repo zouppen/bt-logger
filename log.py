72992d706a6aba056a8b61d0031feef7a3fdb9b6
Joel Lehtonen <joel.lehtonen@iki.fi>
2010-01-22 22:57:51 +0200

    Tuos fixed my code to look like a real python script. Thanks!

--- log.py
+++ log.py
@@ -1,29 +1,21 @@
-import subprocess                                                      
+from __future__ import print_function
+
+import subprocess
 import re                                                              
 
-def sqlesc(str):
-        return str.replace('\'','\'\'')
+def sqlesc(string):
+    return string.replace("'","''")
 
-pat = re.compile('^\t(..:..:..:..:..:..)\t(.*)$')
+if __name__ == "__main__":
+    pipe = subprocess.Popen(["hcitool", "scan"], stdout=subprocess.PIPE)
+    text = pipe.communicate()[0];
 
-text = subprocess.Popen(["hcitool", "scan"], stdout=subprocess.PIPE).communicate
-()[0];
-
-n = 0
-sql = 'insert into log (hwaddr,name) values'
-
-for line in text.splitlines():
-        if n != 0:
-                sql = sql+','
-        matchs = re.match(pat,line)
-        if matchs == None:
-                continue
-        n=n+1
-        sql = sql + '(\''+sqlesc(matchs.group(1))+'\',\''+sqlesc(matchs.group(2)
-)+'\')'
-
-sql = sql + ';'
-
-if n != 0:
-        print sql
-
+    pat = re.compile(r"^\t(..:..:..:..:..:..)\t(.*)$")
+    bd_entries = []
+    for line in text.splitlines():
+        matchs = re.match(pat, line)
+        if matchs is None:
+            continue
+        bd_entries.append("insert into log (hwaddr,name) values('%s','%s');"
+                          % (sqlesc(matchs.group(1)), sqlesc(matchs.group(2))))
+    print("".join(bd_entries))
