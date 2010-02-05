b16cb9fcc69419d6825d7515e4d2e45782470600
Joel Lehtonen <joell@kattila.(none)>
2010-02-05 01:23:02 +0000

    Modified the site script to use SQL directly and to call replicator.

--- log.py
+++ log.py
@@ -2,6 +2,10 @@ from __future__ import print_function
 
 import subprocess
 import re                                                              
+import MySQLdb
+import httplib
+
+# TODO Rewrite this script to build SQL queries with prepare
 
 def sqlesc(string):
     return string.replace("'","''")
@@ -25,5 +29,19 @@ if __name__ == "__main__":
 
     # Propagate temporary table contents to right tables
     sql_entries.append("CALL update_tables;")
-    
-    print("".join(sql_entries))
+
+    conn = MySQLdb.connect (host = "localhost",
+                            user = "logger",
+                            passwd = "loglog",
+                            db = "bluetooth")
+
+    cursor = conn.cursor()
+    cursor.execute ("".join(sql_entries))
+    cursor.close ()
+    conn.close ()
+
+    # replicate
+    repl = httplib.HTTPConnection('192.168.24.1',timeout=60)
+    repl.request('GET','/bt-logger/trigger_replication')
+    print(repl.getresponse().read())
+
